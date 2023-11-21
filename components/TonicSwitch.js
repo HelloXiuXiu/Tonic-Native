5460ac8ec35850699ab8f6b53e0b3a3358fbb1a8
HelloXiuXiu <xiuxiuweb@gmail.com>
2023-11-21 14:59:39 +0100

    plarform and theme controls

--- components/TonicSwitch.js
+++ components/TonicSwitch.js
@@ -3,74 +3,80 @@ import Tonic from '@socketsupply/tonic'
 class TonicSwitch extends Tonic {
   constructor() {
     super()
-    this.state.on = (this.props.default === 'true')
-    //
-    // platform can be read form window.navigator
-    // but also set manually as a prop
-    // in case user want's to keep one style
-    // across all os
-    //
-    // this.state.platform = this.props.platform || window.navigator.....
-    this.state.platform = this.props.platform
+    this.state.active = (this.props.active === 'true' || this.props.active === true)
+    // console.log(this.state.active) // false 
+  }
+
+  connected () {
+    this.state.active = (this.props.active === 'true')
+    this.state.active === true && this.classList.add('active')
+    // console.log(this.state.active) // true
+    this.reRender()
+  }
+
+  get value () {
+    return this.state.active
+  }
+
+  set value (v) {
+    this.state.active = Boolean(v)
   }
 
   click(e) {
     const el = Tonic.match(e.target, '[data-event="toggle"]')
     if (!el) return
 
-    this.state.on = !this.state.on
-    this.reRender()
+    this.state.active = !this.state.active
+    this.classList.toggle('active')
   }
 
   static stylesheet () {
     return `
-    tonic-switch .tonic-switch-wrapper {
-      position: relative;
-      width: 52px;
-      height: 32px;
-      cursor: pointer;
-    }
+      tonic-switch .tonic-switch-wrapper {
+        position: relative;
+        width: 52px;
+        height: 32px;
+        cursor: pointer;
+      }
 
-    tonic-switch .tonic-switch-wrapper .tonic-switch-base {
-      width: 100%;
-      height: 100%;
-      border-radius: 32px;
-    }
+      tonic-switch .tonic-switch-wrapper .tonic-switch-base {
+        width: 100%;
+        height: 100%;
+        border-radius: 32px;
+        width: 100%;
+        background-color: var(--tonic-grey);
+      }
 
-    tonic-switch .tonic-switch-wrapper[on='true'] .tonic-switch-base {
-      background-color: var(--tonic-accent);
-    }
+      tonic-switch.active .tonic-switch-wrapper .tonic-switch-base {
+        background-color: var(--tonic-accent);
+      }
 
-    tonic-switch .tonic-switch-wrapper[on='false'] .tonic-switch-base {
-      width: 100%;
-      background-color: var(--tonic-grey);
-    }
+      tonic-switch .tonic-switch-wrapper .tonic-switch-handle {
+        position: absolute;
+        top: 4px;
+        left: 50%;
+        width: 24px;
+        height: 24px;
+        background-color: var(--tonic-white);
+        border-radius: 32px;
+        transform: translateX(-50%);
+        transform: translateX(-22px);
+        transition: all .2s ease-in-out;
+      }
 
-    tonic-switch .tonic-switch-wrapper .tonic-switch-handle {
-      position: absolute;
-      top: 4px;
-      left: 50%;
-      width: 24px;
-      height: 24px;
-      background-color: var(--tonic-white);
-      border-radius: 32px;
-      transform: translateX(-50%);
-      transition: all .6s ease-in-out;
-    }
+      tonic-switch.active .tonic-switch-wrapper .tonic-switch-handle {
+        transform: translateX(-2px);
+      }
 
-    tonic-switch .tonic-switch-wrapper[on='true'] .tonic-switch-handle {
-      transform: translateX(-2px);
-    }
-
-    tonic-switch .tonic-switch-wrapper[on='false'] .tonic-switch-handle {
-      transform: translateX(-22px);
-    }
+      tonic-switch .tonic-switch-wrapper[on='false'] .tonic-switch-handle {
+        transform: translateX(-22px);
+      }
     `
   }
 
   render () {
     return this.html`
-      <div data-event="toggle" class="tonic-switch-wrapper" on=${this.state.on.toString()}>
+      <div data-event="toggle" class="tonic-switch-wrapper">
         <div class="tonic-switch-handle"></div>
         <div class="tonic-switch-base"></div>
       </div>
--- main.js
+++ main.js
@@ -3,10 +3,60 @@ import'./components/TonicSwitch.js'
 
 import Tonic from '@socketsupply/tonic'
 
+//
+// TODO: get process from socket:process (or navigator?)
+// to check the platform
+// 
+// document.body.setAttribute('platform', process.platform)
+//
+
+function setDefaults(theme, platform) {
+  document.body.setAttribute('platform', platform)
+  document.body.setAttribute('theme', theme)
+
+  let platformEvent = '[data-event="' + platform.toLowerCase() + '"]'
+  let themeEvent = '[data-event="' + theme.toLowerCase() + '"]'
+  document.querySelector(platformEvent).classList.add('active')
+  document.querySelector(themeEvent).classList.add('active')
+}
+
+function handlePlatforms(e) {
+  let trig = e.target.closest('[data-event]')
+  if (!trig) return
+
+  let parent = trig.parentElement
+  let attr = parent.classList.value.split('controls-').join('')
+
+  document.body.setAttribute(attr, trig.dataset.event)
+  
+  Array.from(parent.children).forEach(el => {
+    el.classList.remove('active')
+  })
+  trig.classList.add('active')
+}
+
 document.querySelector('#app').innerHTML = `
+  <div class="controls">
+    <div class="controls-platform">
+      <h3>Platform</h3>
+      <button data-event="android/linux">Android/Linux</button>
+      <button data-event="ios/macos">iOS/macOS</button>
+      <button data-event="windows">Windows</button>
+    </div>
+    <div class="controls-theme">
+      <h3>Theme</h3>
+      <button data-event="light">Light</button>
+      <button data-event="dark">Dark</button>
+    </div>
+  </div>
   <h2>Switch</h2>
   <tonic-switch
     id="tonic-switch" 
-    default="true"
-    platform="ios"></tonic-switch>
+    active="true"
+    platform="android"></tonic-switch>
 `
+
+document.addEventListener("DOMContentLoaded", () => setDefaults('light', 'android/linux'))
+document.querySelector('.controls').addEventListener('click', handlePlatforms)
+
+
