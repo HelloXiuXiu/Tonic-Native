838942a14f6466272b77018edadd8adf7842fb1e
HelloXiuXiu <xiuxiuweb@gmail.com>
2023-11-23 19:25:15 +0100

    color controls added

--- website/components/TonicDevControls.js
+++ website/components/TonicDevControls.js
@@ -3,21 +3,63 @@ import Tonic from '@socketsupply/tonic'
 class TonicDevControls extends Tonic {
   connected () {
     this.setActiveButtons()
+    this.setColorValues()
   }
 
   setActiveButtons () {
     document.addEventListener("DOMContentLoaded", () => {
-      let platform
-      let theme
-
-      platform = document.body.getAttribute('platform')
-      theme = document.body.getAttribute('theme')
+      let platform = document.body.getAttribute('platform')
+      let theme = document.body.getAttribute('theme')
 
       let platformEvent = '[data-event="' + platform.toLowerCase() + '"]'
       let themeEvent = '[data-event="' + theme.toLowerCase() + '"]'
 
       document.querySelector(platformEvent).classList.add('active')
       document.querySelector(themeEvent).classList.add('active')
+    })
+  }
+
+  setColorValues () {
+    document.addEventListener("DOMContentLoaded", () => {
+      let styles = getComputedStyle(document.body)
+
+      let accentColor = styles.getPropertyValue("--tonic-accent")
+      let greyColor = styles.getPropertyValue("--tonic-grey")
+      let greySecondaryColor = styles.getPropertyValue("--tonic-grey-secondary")
+      let whiteColor = styles.getPropertyValue("--tonic-white")
+      let blackColor = styles.getPropertyValue("--tonic-black")
+
+      let accentInp = document.getElementById('var-accent')
+      let greyInp = document.getElementById('var-grey')
+      let greySecondaryInp = document.getElementById('var-grey-secondary')
+      let whiteInp = document.getElementById('var-white')
+      let blackInp = document.getElementById('var-black')
+
+      accentInp.value = accentColor
+      greyInp.value = greyColor
+      greySecondaryInp.value = greySecondaryColor
+      whiteInp.value = whiteColor
+      blackInp.value = blackColor
+
+      accentInp.addEventListener('input', (e) => {
+        document.body.style.setProperty('--tonic-accent', e.target.value)
+      })
+
+      greyInp.addEventListener('input', (e) => {
+        document.body.style.setProperty('--tonic-grey', e.target.value)
+      })
+
+      greySecondaryInp.addEventListener('input', (e) => {
+        document.body.style.setProperty('--tonic-grey-secondary', e.target.value)
+      })
+
+      whiteInp.addEventListener('input', (e) => {
+        document.body.style.setProperty('--tonic-white', e.target.value)
+      })
+
+      blackInp.addEventListener('input', (e) => {
+        document.body.style.setProperty('--tonic-black', e.target.value)
+      })
     })
   }
 
@@ -60,6 +102,24 @@ class TonicDevControls extends Tonic {
       .controls button:hover {
         opacity: 1;
       }
+
+      .controls-color label {
+        font-size: 10px;
+        line-height: 1;
+        vertical-align: top;
+        color: var(--tonic-grey);
+        text-transform: uppercase;
+      }
+
+      .controls-color input {
+        margin-right: 16px;
+        width: 27px;
+        border-radius: 2px;
+      }
+
+      body[theme="dark"] .controls-color input {
+       background-color: transtarent;
+      }
     `
   }
 
@@ -76,6 +136,19 @@ class TonicDevControls extends Tonic {
           <h3>Theme</h3>
           <button data-event="light">Light</button>
           <button data-event="dark">Dark</button>
+        </div>
+        <div class="controls-color">
+          <h3>Theme colors <span>(light theme)</span></h3>
+          <label for="var-accent">Accent</label>
+          <input type="color" id="var-accent" name="var-accent">
+          <label for="var-grey">Grey 1</label>
+          <input type="color" id="var-grey" name="var-grey">
+          <label for="var-grey-secondary">Grey 2</label>
+          <input type="color" id="var-grey-secondary" name="var-grey-secondary">
+          <label for="var-white">White</label>
+          <input type="color" id="var-white" name="var-white">
+          <label for="var-white">Black</label>
+          <input type="color" id="var-black" name="var-black">
         </div>
       </div>
     `
